e059e9aca9fd6ad51bc4968cac52000f9829d709
OzturkBerkan <123197116+OzturkBerkan@users.noreply.github.com>
2023-02-10 11:37:53 -0800

    Update views.py

--- ERFM/en/views.py
+++ ERFM/en/views.py
@@ -11,3 +11,6 @@ def display(request):
 # Create a function that, when a hospital has been selected, shows the estimated wait time and the number of people in queue.
 
 # Create a function that allows a user to update the number of people in queue.
+
+# Currently, when the user goes to our website, they will be asked to share their location, and if they allow their location to be shared, then the map will appear 
+# and their location will be pinged on the map. Next step is to, show all the hospitals that are close to the users' location. 
