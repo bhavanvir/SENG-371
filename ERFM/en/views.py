1044414ab914dfed11d1d52507f14a705d520de8
lsancer99 <80100275+DeweiYu@users.noreply.github.com>
2023-02-10 11:12:49 -0800

    Update views.py

--- ERFM/en/views.py
+++ ERFM/en/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# make a function to make a requst to access html
+# make a function to make a request to access html
 
 def display(request):
     return render(request, 'en.html')
