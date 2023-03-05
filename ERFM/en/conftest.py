500fcfb980a438fc3a3450f6ae792ee055c6e4eb
Jim Kurt Duco <108490686+jimduco@users.noreply.github.com>
2023-03-05 15:27:36 -0800

    Create conftest.py

---
+++ ERFM/en/conftest.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
+from selenium import webdriver
+import unittest
+import selenium
+
+
+@pytest.fixture()
+def setup(request):
+    chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install())
+
+    chrome_options = Options()
+    options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+    for option in options:
+        chrome_options.add_argument(option)
+
+    request.cls.driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+
+    yield request.cls.driver
+    request.cls.driver.close()
+    
+# Modified from https://dev.to/delrayo/executing-selenium-test-with-pythonpytest-using-github-actions-317c
