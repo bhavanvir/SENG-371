131f01653fa27399e4013d99676b867a5714620a
Jim Kurt Duco <108490686+jimduco@users.noreply.github.com>
2023-03-05 17:05:08 -0800

    Update tests.py

--- ERFM/en/tests.py
+++ ERFM/en/tests.py
@@ -3,6 +3,12 @@ from django.test import TestCase
 from selenium import webdriver
 import unittest
 import selenium
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
 # Create your tests here.
 @pytest.mark.usefixtures("setup")
@@ -10,7 +16,7 @@ class tests(unittest.TestCase):
 
     def test_page_title_check(self):
 
-        chrome_driver = webdriver.Chrome()
+        chrome_driver = webdriver.Chrome('/home/<user>/chromedriver',chrome_options=chrome_options)
         chrome_driver.get("http://127.0.0.1:8000/")
         first_value = chrome_driver.title
         second_value = "Emergency Room Flow Manager"
