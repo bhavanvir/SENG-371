2fcabc2fce89e90306a622aabd98d7a1870995ad
Bhavanvir Rai <bhavanvir.r@gmail.com>
2023-03-17 21:09:37 -0700

    Added further styling to pop-ups, as well as incremental buttons.

--- ERFM/static/hospital.js
+++ ERFM/static/hospital.js
@@ -1,24 +1,97 @@
 function hospitalPopUpFunction(hospital) {
-    var divElement = document.createElement('div')
-    var container = document.createElement("span")
-    var texthere = document.createTextNode(hospital)
-    var texthere1 = document.createTextNode("Non-emergency wait: 1 hr 35 min")
-    var texthere2 = document.createTextNode("People ahead of you: 15    ?")
-    container.appendChild(texthere)
-    container.appendChild(document.createElement("br"))
-    container.appendChild(texthere1)
-    container.appendChild(document.createElement("br"))
-    container.appendChild(texthere2)
-    container.style.color = "black"
-    divElement.appendChild(container)
-    divElement.style.width = "600px"
-    divElement.style.height = "90px"
-    divElement.style.background = "white"
-    divElement.style.position = "fixed"
-    divElement.style.left = "40%"
-    divElement.style.right = "50%"
-    divElement.style.bottom = "20px"
-    divElement.style.margin = "0 auto"
-    divElement.style.padding = "10px 50px 5px 10px"
-    document.body.appendChild(divElement)
+    var cardElement = document.createElement('div');
+    cardElement.classList.add('card');
+
+    var container = document.createElement("span");
+
+    // Create first list item with bold hospital name
+    var li1Element = document.createElement('li');
+    li1Element.classList.add('list-group-item');
+    var boldElement = document.createElement('b');
+    var texthere = document.createTextNode(hospital);
+    boldElement.appendChild(texthere);
+    li1Element.appendChild(boldElement);
+
+    // Create second list item with non-emergency wait, emergency wait, and people ahead of you
+    var li2Element = document.createElement('li');
+    li2Element.classList.add('list-group-item');
+
+    var randomHour1 = Math.floor(Math.random() * 4);
+    var randomHour2 = Math.floor(Math.random() * 4);
+    var randomMinute1 = Math.floor(Math.random() * 60);
+    var randomMinute2 = Math.floor(Math.random() * 60);
+    var randomPeople1 = Math.floor(Math.random() * 50);
+    var randomPeople2 = Math.floor(Math.random() * 50);
+
+    var nonEmergencyWait = document.createTextNode("Non-emergency wait: " + randomHour1 + " hr " + randomMinute1 + " min");
+    var emergencyWait = document.createTextNode("Emergency wait: " + randomHour2 + " hr " + randomMinute2 + " min");
+    var peopleAheadNonEmergency = document.createTextNode("People ahead of you: " + randomPeople1);
+    var peopleAheadEmergency = document.createTextNode("People ahead of you: " + randomPeople2);
+
+    var nonEmergencyIncrButton = document.createElement('button');
+    nonEmergencyIncrButton.innerText = '+';
+    nonEmergencyIncrButton.classList.add('btn', 'btn-success', 'mx-1', 'py-0');
+    nonEmergencyIncrButton.style.width = "50px";
+    nonEmergencyIncrButton.addEventListener('click', function() {
+        randomPeople1 += 1;
+        peopleAheadNonEmergency.nodeValue = "People ahead of you: " + randomPeople1;
+    });
+    
+    var emergencyIncrButton = document.createElement('button');
+    emergencyIncrButton.innerText = '+';
+    emergencyIncrButton.classList.add('btn', 'btn-success', 'mx-1', 'py-0');
+    emergencyIncrButton.style.width = "50px";
+    emergencyIncrButton.addEventListener('click', function() {
+        randomPeople2 += 1;
+        peopleAheadEmergency.nodeValue = "People ahead of you: " + randomPeople2;
+    });
+    
+    var nonEmergencyDecButton = document.createElement('button');
+    nonEmergencyDecButton.innerText = '-';
+    nonEmergencyDecButton.classList.add('btn', 'btn-danger', 'mx-1', 'py-0');
+    nonEmergencyDecButton.style.width = "50px";
+    nonEmergencyDecButton.addEventListener('click', function() {
+        randomPeople1 = Math.max(randomPeople1 - 1, 0);
+        peopleAheadNonEmergency.nodeValue = "People ahead of you: " + randomPeople1;
+    });
+    
+    var emergencyDecButton = document.createElement('button');
+    emergencyDecButton.innerText = '-';
+    emergencyDecButton.classList.add('btn', 'btn-danger', 'mx-1', 'py-0');
+    emergencyDecButton.style.width = "50px";
+    emergencyDecButton.addEventListener('click', function() {
+        randomPeople2 = Math.max(randomPeople2 - 1, 0);
+        peopleAheadEmergency.nodeValue = "People ahead of you: " + randomPeople2;
+    });
+
+    li2Element.appendChild(nonEmergencyWait);
+    li2Element.appendChild(document.createElement("br"));
+    li2Element.appendChild(peopleAheadNonEmergency);
+    li2Element.appendChild(nonEmergencyIncrButton);
+    li2Element.appendChild(nonEmergencyDecButton);
+    li2Element.appendChild(document.createElement("br"));
+    li2Element.appendChild(document.createElement("br"));
+    li2Element.appendChild(emergencyWait);
+    li2Element.appendChild(document.createElement("br"));
+    li2Element.appendChild(peopleAheadEmergency);
+    li2Element.appendChild(emergencyIncrButton);
+    li2Element.appendChild(emergencyDecButton);
+    
+    // Append both list items to the list
+    var ulElement = document.createElement('ul');
+    ulElement.classList.add('list-group', 'list-group-flush');
+    ulElement.appendChild(li1Element);
+    ulElement.appendChild(li2Element);
+
+    container.appendChild(ulElement);
+    cardElement.appendChild(container);
+
+    var width = 400;
+    var algin = (screen.width-width)/2;
+    cardElement.style.width = width + "px";
+    cardElement.style.position = "fixed";
+    cardElement.style.right = algin + "px";
+    cardElement.style.bottom = "5px";
+
+    document.body.appendChild(cardElement);
 }
